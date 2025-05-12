6a80a35173ded81e889d3bb228430c8213095ecf
Ali <5868653+ahaydarli@users.noreply.github.com>
2025-05-12 01:39:07 -0700

    Update TokenResponse.swift

--- WhelpChat/Classes/TokenResponse.swift
+++ WhelpChat/Classes/TokenResponse.swift
@@ -9,13 +9,36 @@ import Foundation
 
 struct TokenResponse : Decodable {
     var token: String?
+    var url: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case url
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.url = try? container.decode(String.self, forKey: .url)
+        
+        if let urlString = self.url,
+           let url = URL(string: urlString),
+           let components = URLComponents(url: url, resolvingAgainstBaseURL: false),
+           let token = components.queryItems?.first(where: { $0.name == "token" })?.value {
+            self.token = token
+        } else {
+            self.token = nil
+        }
+    }
     
     var loadableUrl: URL? {
+        if let urlString = url, let directUrl = URL(string: urlString) {
+            return directUrl
+        }
+        
         if let token = token {
-            let fullPath = String(format: "%@%@", "https://widget.whelp.co/sdk/chat?token=",token)
-            
+            let fullPath = String(format: "%@%@", "https://widget.whelp.co/sdk/chat?token=", token)
             return URL(string: fullPath)
         }
+        
         return nil
     }
 }
